942a1f99c0600e7b4d08f75d21f70597173524be
mithanckm <39883935+mithanckm@users.noreply.github.com>
2021-02-22 01:06:26 -0500

    Update functiongenerator.js

--- functiongenerator.js
+++ functiongenerator.js
@@ -1,1 +1,14 @@
-ggg
+function* Generator(){
+
+    yield 8;
+    yield 9;
+    return 90;
+}
+
+
+
+var generate  = Generator();
+
+console.log(generate.next());
+console.log(generate.next());
+console.log(generate.next());
